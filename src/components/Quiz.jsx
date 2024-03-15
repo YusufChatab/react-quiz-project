301f2d25c12109246206f43df2bfcf7326342bc6
Muhammad Yusuf Chatab <ychatab@gmail.com>
2024-03-15 16:08:54 +0800

    first commit

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,39 @@
+import { useState, useCallback } from "react";
+import QUESTIONS from "../questions";
+import Question from "./Question";
+import Summary from "./Summary";
+
+export default function Quiz() {
+  const [userAnswer, setUserAnswer] = useState([]);
+
+  const activeQuestionIndex = userAnswer.length;
+
+  const quizCompleted = activeQuestionIndex === QUESTIONS.length;
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswer((prevUserAnswer) => {
+      return [...prevUserAnswer, selectedAnswer];
+    });
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(() => {
+    handleSelectAnswer(null);
+  }, [handleSelectAnswer]);
+
+  if (quizCompleted) {
+    return <Summary userAnswer={userAnswer} />;
+  }
+
+  return (
+    <div id="quiz">
+      <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
+    </div>
+  );
+}
---
+++ src/components/Summary.jsx
@@ -0,0 +1,61 @@
+import quizCompleteImg from "../assets/quiz-complete.png";
+import QUESTIONS from "../questions";
+
+export default function Summary({ userAnswer }) {
+  const skippedAnswers = userAnswer.filter((answer) => answer === null);
+  const correctAnswers = userAnswer.filter(
+    (answer, index) => answer === QUESTIONS[index].answers[0]
+  );
+
+  const skippedAnswerShare = Math.round(
+    (skippedAnswers.length / userAnswer.length) * 100
+  );
+
+  const correctAnswerShare = Math.round(
+    (correctAnswers.length / userAnswer.length) * 100
+  );
+
+  const wrongAnswerShare = 100 - skippedAnswerShare - correctAnswerShare;
+
+  return (
+    <div id="summary">
+      <img src={quizCompleteImg} alt="Trophy Icon" />
+      <h2>Quiz Completed</h2>
+      <div id="summary-stats">
+        <p>
+          <span className="number">{skippedAnswerShare}%</span>
+          <span className="text">skipped</span>
+        </p>
+        <p>
+          <span className="number">{correctAnswerShare}%</span>
+          <span className="text">answered correctly</span>
+        </p>
+        <p>
+          <span className="number">{wrongAnswerShare}%</span>
+          <span className="text">answered incorrectly</span>
+        </p>
+      </div>
+      <ol>
+        {userAnswer.map((answer, index) => {
+          let cssClasses = "user-answer";
+
+          if (answer === null) {
+            cssClasses += " skipped";
+          } else if (answer === QUESTIONS[index].answers[0]) {
+            cssClasses += " correct";
+          } else {
+            cssClasses += " wrong";
+          }
+
+          return (
+            <li key={index}>
+              <h3>{index + 1}</h3>
+              <p className="question">{QUESTIONS[index].text}</p>
+              <p className={cssClasses}>{answer ?? "Skipped"}</p>
+            </li>
+          );
+        })}
+      </ol>
+    </div>
+  );
+}
